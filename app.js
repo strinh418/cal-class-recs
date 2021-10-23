035987794a875f5e29ca34283d6037e3b421b53d
David Lin <david.lin7303@gmail.com>
2021-10-23 13:44:17 -0700

    Trying to get heroku site to run

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const app = require("express")(); 
+const cool = require('cool-ascii-faces');
 
 const PORT = process.env.PORT || 3000; 
 
 app.get("", (req, res) => {
-	res.send("Hello world"); 
-}); 
+	res.send("Hello world Boys"); 
+}).get('/cool', (req, res) => res.send(cool()));
 
 app.listen(PORT, () => {
 	console.log(`App up at port ${PORT}`); 
