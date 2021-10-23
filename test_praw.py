3d21e2ecb8545e92d9eda11adc27b19609b9c2c0
connermanuel <manuel.conner.g@gmail.com>
2021-10-22 21:12:00 -0700

    add sample.env

--- test_praw.py
+++ test_praw.py
@@ -1,8 +1,16 @@
 import praw
 import typing
 
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
 def scrape_class(class_name: str) -> None:
     """
     Given the name of a class, return something...
     """
-    pass
+    reddit = praw.Reddit(
+        client_id = os.getenv('CLIENT_ID'),
+        client_secret = os.getenv('CLIENT_SECRET'),
+        user_agent = os.getenv('USER_AGENT')
+    )
